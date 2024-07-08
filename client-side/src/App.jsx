fb1e80235e37f38816e5f5cd6993e7a77bf8d926
Rashedul Hassan Sabbir <rashedulhasan1996@gmail.com>
2024-07-08 22:30:49 +0600

    text changed

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -2,16 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="bg-white flex flex-row justify-center">
+      <div className="segment">
+        <h1 className="text-2xl text-black font-bold">
+          YouTube Video Downloader
+        </h1>
+        <h3 className="text-sm text-black font-mono">
+          Paste the YouTube video URL and instantly download videos for offline
+          viewing.
+        </h3>
+      </div>
       <form className="bg-gray-100 p-4 rounded-lg shadow-lg max-w-xs mx-auto">
-        <div className="segment">
-          <h1 className="text-2xl text-black font-bold">Sign up</h1>
-        </div>
-
         <label className="block mb-6">
           <input
             type="text"
-            placeholder="Email Address"
+            placeholder="Paste your video link here"
             className="neumorphic-input"
           />
         </label>
@@ -20,10 +25,10 @@ function App() {
           className="neumorphic-button text-red-700 font-bold"
           type="button"
         >
-          <i className="icon ion-md-lock mr-2"></i> Log in
+          <i className="icon ion-md-lock mr-2"></i>Download
         </button>
       </form>
-    </>
+    </div>
   );
 }
 
