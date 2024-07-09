6a727bf8df8ae051c4813493e3ec99c70601309d
rashedsabbir <rashedulhasan1996@gmail.com>
2024-07-09 22:36:18 +0600

    text centered

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -2,18 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-white flex flex-row justify-center">
-      <div className="segment">
-        <img className="w-16 h-16" src="../public/youtube.svg" alt="" />
-        <h1 className="text-2xl text-black font-bold">
-          YouTube Video Downloader
-        </h1>
-        <h3 className="text-sm text-black font-mono">
-          Paste the YouTube video URL and instantly download videos for offline
-          viewing.
-        </h3>
-      </div>
-      <form className="bg-gray-100 p-4 rounded-lg shadow-lg max-w-xs mx-auto">
+    <div className="bg-white">
+      <form className="bg-gray-100 p-4 rounded-lg shadow-lg max-w-3xl mx-auto flex items-center flex-col">
+        <div className="flex flex-col items-center">
+          <img className="w-16 h-16" src="../public/youtube.svg" alt="" />
+          <h1 className="text-2xl text-black text-center font-bold">
+            YouTube Video Downloader
+          </h1>
+          <h3 className="text-sm text-black text-center font-mono">
+            Paste the YouTube video URL and instantly download videos for
+            offline viewing.
+          </h3>
+        </div>
         <label className="block mb-6">
           <input
             type="text"
