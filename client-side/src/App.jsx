731440b43e888069753913399ee1a6778c793b6b
rashedsabbir <rashedulhasan1996@gmail.com>
2024-07-10 15:13:36 +0600

    responsive added

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -2,31 +2,28 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-white">
-      <form className="bg-gray-100 p-4 rounded-lg shadow-lg max-w-3xl mx-auto flex items-center flex-col">
-        <div className="flex flex-col items-center">
+    <div className="py-8">
+      <form className="bg-gray-200 p-4 m-16 rounded-lg shadow-lg max-w-3xl mx-auto flex items-center flex-col">
+        <div className="grid grid-cols-1 gap-6 place-items-center">
           <img className="w-16 h-16" src="../public/youtube.svg" alt="" />
-          <h1 className="text-2xl text-black text-center font-bold">
+          <p className="text-4xl text-black text-center font-bold">
             YouTube Video Downloader
-          </h1>
-          <h3 className="text-sm text-black text-center font-mono">
+          </p>
+          <p className="text-md pb-6 text-black text-center">
             Paste the YouTube video URL and instantly download videos for
             offline viewing.
-          </h3>
+          </p>
         </div>
         <label className="block mb-6">
           <input
             type="text"
             placeholder="Paste your video link here"
-            className="neumorphic-input"
+            className="neumorphic-input w-96 text-black"
           />
         </label>
 
-        <button
-          className="neumorphic-button text-red-700 font-bold"
-          type="button"
-        >
-          <i className="icon ion-md-lock mr-2"></i>Download
+        <button className="neumorphic-button font-bold" type="button">
+          Download
         </button>
       </form>
     </div>
