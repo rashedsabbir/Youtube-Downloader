f63592aeb828e25d6ad32d3f87cffec7c308223e
rashedsabbir <rashedulhasan1996@gmail.com>
2024-07-11 23:50:39 +0600

    modal added

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -18,10 +18,10 @@ function App() {
           <input
             type="text"
             placeholder="Paste your video link here"
-            className="neumorphic-input w-96 p-4 placeholder-pl-4 text-black bg-gray-200 rounded-lg shadow-lg focus:outline-none focus:ring-gray-300"
+            className="neumorphic-input w-96 p-4 placeholder:pl-6 text-black bg-gray-200 rounded-lg shadow-lg focus:outline-none focus:ring-gray-300"
           />
         </label>
-        <button className="neumorphic-button font-bold w-32 p-4 bg-red-500 text-gray-100 rounded-lg shadow-lg focus:outline-none hover:border-0">
+        <button className="neumorphic-button font-bold w-32 p-4 bg-red-500 text-gray-100 rounded-lg shadow-lg shadow-red-300 focus:outline-none hover:border-0 focus:ring-gray-300">
           Download
         </button>
       </form>
